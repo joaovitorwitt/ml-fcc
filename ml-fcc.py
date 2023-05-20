831e835808555720ea90e9c431a555ff0c6f70e5
João Vitor Witt <joaovitorwitt2004@gmail.com>
2023-05-20 18:48:39 -0300

    added labels

--- ml-fcc.py
+++ ml-fcc.py
@@ -2,5 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+cols = ["fLength", "fWidth" "fSize", "fConc", "fConc1", "fAsym", "fM3Long", "fM3Trans", "fAlpha", "fDist", "class"]
+
 file = pd.read_csv("sample-data/magic04.data")
 print(file)
